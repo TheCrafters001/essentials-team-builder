fb13b2e53bb3891b9b7fe8378a1c9a14f5d2ae4f
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2020-08-11 12:43:28 -0400

    Treecko

--- src/Showdown to Essentials Converter/Form1.Designer.vb
+++ src/Showdown to Essentials Converter/Form1.Designer.vb
@@ -22,30 +22,57 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Me.TabControl1 = New System.Windows.Forms.TabControl()
         Me.pg_Trainer = New System.Windows.Forms.TabPage()
+        Me.trainerName_lbl = New System.Windows.Forms.Label()
+        Me.trainerName_txtBox = New System.Windows.Forms.TextBox()
+        Me.trainerInfoTitle_lbl = New System.Windows.Forms.Label()
         Me.pg_POne = New System.Windows.Forms.TabPage()
         Me.pg_PTwo = New System.Windows.Forms.TabPage()
         Me.pg_PThree = New System.Windows.Forms.TabPage()
         Me.pg_PFour = New System.Windows.Forms.TabPage()
         Me.pg_PFive = New System.Windows.Forms.TabPage()
         Me.pg_PSix = New System.Windows.Forms.TabPage()
+        Me.pg_Output = New System.Windows.Forms.TabPage()
         Me.pg_About = New System.Windows.Forms.TabPage()
         Me.aboutText_lbl = New System.Windows.Forms.Label()
         Me.gnuGPL3_picBox = New System.Windows.Forms.PictureBox()
         Me.aboutAuthor_lbl = New System.Windows.Forms.Label()
         Me.aboutTitle_lbl = New System.Windows.Forms.Label()
         Me.Logo_picBox = New System.Windows.Forms.PictureBox()
-        Me.pg_Output = New System.Windows.Forms.TabPage()
-        Me.trainerInfoTitle_lbl = New System.Windows.Forms.Label()
-        Me.trainerName_txtBox = New System.Windows.Forms.TextBox()
-        Me.trainerName_lbl = New System.Windows.Forms.Label()
+        Me.existingBattle_chkBox = New System.Windows.Forms.CheckBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.battleTeamID_lbl = New System.Windows.Forms.Label()
+        Me.battleTeamID_txtBox = New System.Windows.Forms.TextBox()
+        Me.trainerType_lbl = New System.Windows.Forms.Label()
+        Me.trainerType_txtBox = New System.Windows.Forms.TextBox()
+        Me.trainerItems_grp = New System.Windows.Forms.GroupBox()
+        Me.trainerUsesItems_chkBox = New System.Windows.Forms.CheckBox()
+        Me.numPoké_cmb = New System.Windows.Forms.ComboBox()
+        Me.numPoké_lbl = New System.Windows.Forms.Label()
+        Me.properSpellingNote_lbl = New System.Windows.Forms.Label()
+        Me.itemOne_txtBox = New System.Windows.Forms.TextBox()
+        Me.itemTwo_txtBox = New System.Windows.Forms.TextBox()
+        Me.itemThree_txtBox = New System.Windows.Forms.TextBox()
+        Me.itemFour_txtBox = New System.Windows.Forms.TextBox()
+        Me.itemFive_txtBox = New System.Windows.Forms.TextBox()
+        Me.itemSix_txtBox = New System.Windows.Forms.TextBox()
+        Me.itemSeven_txtBox = New System.Windows.Forms.TextBox()
+        Me.itemEight_txtBox = New System.Windows.Forms.TextBox()
+        Me.outputBox_rchBox = New System.Windows.Forms.RichTextBox()
+        Me.generate_btn = New System.Windows.Forms.Button()
+        Me.save_btn = New System.Windows.Forms.Button()
+        Me.multiOutput_chkBox = New System.Windows.Forms.CheckBox()
+        Me.codeExporter_dialog = New System.Windows.Forms.SaveFileDialog()
         Me.TabControl1.SuspendLayout()
         Me.pg_Trainer.SuspendLayout()
+        Me.pg_Output.SuspendLayout()
         Me.pg_About.SuspendLayout()
         CType(Me.gnuGPL3_picBox, System.ComponentModel.ISupportInitialize).BeginInit()
         CType(Me.Logo_picBox, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.trainerItems_grp.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
@@ -67,6 +94,16 @@ Partial Class Form1
         '
         'pg_Trainer
         '
+        Me.pg_Trainer.Controls.Add(Me.properSpellingNote_lbl)
+        Me.pg_Trainer.Controls.Add(Me.numPoké_lbl)
+        Me.pg_Trainer.Controls.Add(Me.numPoké_cmb)
+        Me.pg_Trainer.Controls.Add(Me.trainerUsesItems_chkBox)
+        Me.pg_Trainer.Controls.Add(Me.trainerItems_grp)
+        Me.pg_Trainer.Controls.Add(Me.trainerType_lbl)
+        Me.pg_Trainer.Controls.Add(Me.trainerType_txtBox)
+        Me.pg_Trainer.Controls.Add(Me.battleTeamID_lbl)
+        Me.pg_Trainer.Controls.Add(Me.battleTeamID_txtBox)
+        Me.pg_Trainer.Controls.Add(Me.existingBattle_chkBox)
         Me.pg_Trainer.Controls.Add(Me.trainerName_lbl)
         Me.pg_Trainer.Controls.Add(Me.trainerName_txtBox)
         Me.pg_Trainer.Controls.Add(Me.trainerInfoTitle_lbl)
@@ -77,6 +114,33 @@ Partial Class Form1
         Me.pg_Trainer.TabIndex = 0
         Me.pg_Trainer.Text = "Trainer Info"
         Me.pg_Trainer.UseVisualStyleBackColor = True
+        '
+        'trainerName_lbl
+        '
+        Me.trainerName_lbl.AutoSize = True
+        Me.trainerName_lbl.Location = New System.Drawing.Point(6, 39)
+        Me.trainerName_lbl.Name = "trainerName_lbl"
+        Me.trainerName_lbl.Size = New System.Drawing.Size(74, 13)
+        Me.trainerName_lbl.TabIndex = 2
+        Me.trainerName_lbl.Text = "Trainer Name"
+        '
+        'trainerName_txtBox
+        '
+        Me.trainerName_txtBox.Location = New System.Drawing.Point(6, 55)
+        Me.trainerName_txtBox.MaxLength = 25
+        Me.trainerName_txtBox.Name = "trainerName_txtBox"
+        Me.trainerName_txtBox.Size = New System.Drawing.Size(127, 22)
+        Me.trainerName_txtBox.TabIndex = 1
+        '
+        'trainerInfoTitle_lbl
+        '
+        Me.trainerInfoTitle_lbl.AutoSize = True
+        Me.trainerInfoTitle_lbl.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.trainerInfoTitle_lbl.Location = New System.Drawing.Point(6, 3)
+        Me.trainerInfoTitle_lbl.Name = "trainerInfoTitle_lbl"
+        Me.trainerInfoTitle_lbl.Size = New System.Drawing.Size(127, 30)
+        Me.trainerInfoTitle_lbl.TabIndex = 0
+        Me.trainerInfoTitle_lbl.Text = "Trainer Info"
         '
         'pg_POne
         '
@@ -132,6 +196,19 @@ Partial Class Form1
         Me.pg_PSix.TabIndex = 6
         Me.pg_PSix.Text = "Pokémon 6"
         Me.pg_PSix.UseVisualStyleBackColor = True
+        '
+        'pg_Output
+        '
+        Me.pg_Output.Controls.Add(Me.multiOutput_chkBox)
+        Me.pg_Output.Controls.Add(Me.save_btn)
+        Me.pg_Output.Controls.Add(Me.generate_btn)
+        Me.pg_Output.Controls.Add(Me.outputBox_rchBox)
+        Me.pg_Output.Location = New System.Drawing.Point(4, 22)
+        Me.pg_Output.Name = "pg_Output"
+        Me.pg_Output.Size = New System.Drawing.Size(752, 511)
+        Me.pg_Output.TabIndex = 8
+        Me.pg_Output.Text = "Output"
+        Me.pg_Output.UseVisualStyleBackColor = True
         '
         'pg_About
         '
@@ -196,41 +273,208 @@ Partial Class Form1
         Me.Logo_picBox.TabIndex = 5
         Me.Logo_picBox.TabStop = False
         '
-        'pg_Output
+        'existingBattle_chkBox
         '
-        Me.pg_Output.Location = New System.Drawing.Point(4, 22)
-        Me.pg_Output.Name = "pg_Output"
-        Me.pg_Output.Size = New System.Drawing.Size(752, 511)
-        Me.pg_Output.TabIndex = 8
-        Me.pg_Output.Text = "Output"
-        Me.pg_Output.UseVisualStyleBackColor = True
+        Me.existingBattle_chkBox.AutoSize = True
+        Me.existingBattle_chkBox.Location = New System.Drawing.Point(139, 57)
+        Me.existingBattle_chkBox.Name = "existingBattle_chkBox"
+        Me.existingBattle_chkBox.Size = New System.Drawing.Size(262, 17)
+        Me.existingBattle_chkBox.TabIndex = 3
+        Me.existingBattle_chkBox.Text = "This team is for a NPC that already has a team."
+        Me.existingBattle_chkBox.UseVisualStyleBackColor = True
         '
-        'trainerInfoTitle_lbl
+        'Timer1
         '
-        Me.trainerInfoTitle_lbl.AutoSize = True
-        Me.trainerInfoTitle_lbl.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.trainerInfoTitle_lbl.Location = New System.Drawing.Point(6, 3)
-        Me.trainerInfoTitle_lbl.Name = "trainerInfoTitle_lbl"
-        Me.trainerInfoTitle_lbl.Size = New System.Drawing.Size(127, 30)
-        Me.trainerInfoTitle_lbl.TabIndex = 0
-        Me.trainerInfoTitle_lbl.Text = "Trainer Info"
         '
-        'trainerName_txtBox
+        'battleTeamID_lbl
         '
-        Me.trainerName_txtBox.Location = New System.Drawing.Point(6, 55)
-        Me.trainerName_txtBox.MaxLength = 25
-        Me.trainerName_txtBox.Name = "trainerName_txtBox"
-        Me.trainerName_txtBox.Size = New System.Drawing.Size(127, 22)
-        Me.trainerName_txtBox.TabIndex = 1
+        Me.battleTeamID_lbl.AutoSize = True
+        Me.battleTeamID_lbl.Location = New System.Drawing.Point(6, 161)
+        Me.battleTeamID_lbl.Name = "battleTeamID_lbl"
+        Me.battleTeamID_lbl.Size = New System.Drawing.Size(182, 13)
+        Me.battleTeamID_lbl.TabIndex = 5
+        Me.battleTeamID_lbl.Text = "Battle Team ID (Must be a number)"
         '
-        'trainerName_lbl
+        'battleTeamID_txtBox
         '
-        Me.trainerName_lbl.AutoSize = True
-        Me.trainerName_lbl.Location = New System.Drawing.Point(6, 39)
-        Me.trainerName_lbl.Name = "trainerName_lbl"
-        Me.trainerName_lbl.Size = New System.Drawing.Size(74, 13)
-        Me.trainerName_lbl.TabIndex = 2
-        Me.trainerName_lbl.Text = "Trainer Name"
+        Me.battleTeamID_txtBox.Location = New System.Drawing.Point(6, 177)
+        Me.battleTeamID_txtBox.MaxLength = 25
+        Me.battleTeamID_txtBox.Name = "battleTeamID_txtBox"
+        Me.battleTeamID_txtBox.Size = New System.Drawing.Size(127, 22)
+        Me.battleTeamID_txtBox.TabIndex = 4
+        '
+        'trainerType_lbl
+        '
+        Me.trainerType_lbl.AutoSize = True
+        Me.trainerType_lbl.Location = New System.Drawing.Point(6, 80)
+        Me.trainerType_lbl.Name = "trainerType_lbl"
+        Me.trainerType_lbl.Size = New System.Drawing.Size(198, 13)
+        Me.trainerType_lbl.TabIndex = 7
+        Me.trainerType_lbl.Text = "Trainer Type (Refer to trainertypes.txt)"
+        '
+        'trainerType_txtBox
+        '
+        Me.trainerType_txtBox.Location = New System.Drawing.Point(6, 96)
+        Me.trainerType_txtBox.MaxLength = 25
+        Me.trainerType_txtBox.Name = "trainerType_txtBox"
+        Me.trainerType_txtBox.Size = New System.Drawing.Size(127, 22)
+        Me.trainerType_txtBox.TabIndex = 6
+        '
+        'trainerItems_grp
+        '
+        Me.trainerItems_grp.Controls.Add(Me.itemEight_txtBox)
+        Me.trainerItems_grp.Controls.Add(Me.itemSeven_txtBox)
+        Me.trainerItems_grp.Controls.Add(Me.itemSix_txtBox)
+        Me.trainerItems_grp.Controls.Add(Me.itemFive_txtBox)
+        Me.trainerItems_grp.Controls.Add(Me.itemFour_txtBox)
+        Me.trainerItems_grp.Controls.Add(Me.itemThree_txtBox)
+        Me.trainerItems_grp.Controls.Add(Me.itemTwo_txtBox)
+        Me.trainerItems_grp.Controls.Add(Me.itemOne_txtBox)
+        Me.trainerItems_grp.Location = New System.Drawing.Point(533, 29)
+        Me.trainerItems_grp.Name = "trainerItems_grp"
+        Me.trainerItems_grp.Size = New System.Drawing.Size(213, 476)
+        Me.trainerItems_grp.TabIndex = 8
+        Me.trainerItems_grp.TabStop = False
+        Me.trainerItems_grp.Text = "Items"
+        '
+        'trainerUsesItems_chkBox
+        '
+        Me.trainerUsesItems_chkBox.AutoSize = True
+        Me.trainerUsesItems_chkBox.Location = New System.Drawing.Point(533, 6)
+        Me.trainerUsesItems_chkBox.Name = "trainerUsesItems_chkBox"
+        Me.trainerUsesItems_chkBox.Size = New System.Drawing.Size(140, 17)
+        Me.trainerUsesItems_chkBox.TabIndex = 9
+        Me.trainerUsesItems_chkBox.Text = "This trainer uses items"
+        Me.trainerUsesItems_chkBox.UseVisualStyleBackColor = True
+        '
+        'numPoké_cmb
+        '
+        Me.numPoké_cmb.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList
+        Me.numPoké_cmb.FormattingEnabled = True
+        Me.numPoké_cmb.Items.AddRange(New Object() {"1", "2", "3", "4", "5", "6"})
+        Me.numPoké_cmb.Location = New System.Drawing.Point(6, 137)
+        Me.numPoké_cmb.Name = "numPoké_cmb"
+        Me.numPoké_cmb.Size = New System.Drawing.Size(127, 21)
+        Me.numPoké_cmb.TabIndex = 10
+        '
+        'numPoké_lbl
+        '
+        Me.numPoké_lbl.AutoSize = True
+        Me.numPoké_lbl.Location = New System.Drawing.Point(6, 121)
+        Me.numPoké_lbl.Name = "numPoké_lbl"
+        Me.numPoké_lbl.Size = New System.Drawing.Size(113, 13)
+        Me.numPoké_lbl.TabIndex = 11
+        Me.numPoké_lbl.Text = "Number of Pokémon"
+        '
+        'properSpellingNote_lbl
+        '
+        Me.properSpellingNote_lbl.AutoSize = True
+        Me.properSpellingNote_lbl.Font = New System.Drawing.Font("Segoe UI", 11.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.properSpellingNote_lbl.Location = New System.Drawing.Point(7, 485)
+        Me.properSpellingNote_lbl.Name = "properSpellingNote_lbl"
+        Me.properSpellingNote_lbl.Size = New System.Drawing.Size(241, 20)
+        Me.properSpellingNote_lbl.TabIndex = 12
+        Me.properSpellingNote_lbl.Text = "Note: Proper spelling IS required."
+        '
+        'itemOne_txtBox
+        '
+        Me.itemOne_txtBox.Location = New System.Drawing.Point(6, 26)
+        Me.itemOne_txtBox.Name = "itemOne_txtBox"
+        Me.itemOne_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemOne_txtBox.TabIndex = 0
+        '
+        'itemTwo_txtBox
+        '
+        Me.itemTwo_txtBox.Location = New System.Drawing.Point(6, 54)
+        Me.itemTwo_txtBox.Name = "itemTwo_txtBox"
+        Me.itemTwo_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemTwo_txtBox.TabIndex = 1
+        '
+        'itemThree_txtBox
+        '
+        Me.itemThree_txtBox.Location = New System.Drawing.Point(6, 82)
+        Me.itemThree_txtBox.Name = "itemThree_txtBox"
+        Me.itemThree_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemThree_txtBox.TabIndex = 2
+        '
+        'itemFour_txtBox
+        '
+        Me.itemFour_txtBox.Location = New System.Drawing.Point(6, 110)
+        Me.itemFour_txtBox.Name = "itemFour_txtBox"
+        Me.itemFour_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemFour_txtBox.TabIndex = 3
+        '
+        'itemFive_txtBox
+        '
+        Me.itemFive_txtBox.Location = New System.Drawing.Point(6, 138)
+        Me.itemFive_txtBox.Name = "itemFive_txtBox"
+        Me.itemFive_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemFive_txtBox.TabIndex = 4
+        '
+        'itemSix_txtBox
+        '
+        Me.itemSix_txtBox.Location = New System.Drawing.Point(6, 166)
+        Me.itemSix_txtBox.Name = "itemSix_txtBox"
+        Me.itemSix_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemSix_txtBox.TabIndex = 5
+        '
+        'itemSeven_txtBox
+        '
+        Me.itemSeven_txtBox.Location = New System.Drawing.Point(6, 194)
+        Me.itemSeven_txtBox.Name = "itemSeven_txtBox"
+        Me.itemSeven_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemSeven_txtBox.TabIndex = 6
+        '
+        'itemEight_txtBox
+        '
+        Me.itemEight_txtBox.Location = New System.Drawing.Point(6, 222)
+        Me.itemEight_txtBox.Name = "itemEight_txtBox"
+        Me.itemEight_txtBox.Size = New System.Drawing.Size(201, 22)
+        Me.itemEight_txtBox.TabIndex = 7
+        '
+        'outputBox_rchBox
+        '
+        Me.outputBox_rchBox.DetectUrls = False
+        Me.outputBox_rchBox.Location = New System.Drawing.Point(3, 3)
+        Me.outputBox_rchBox.Name = "outputBox_rchBox"
+        Me.outputBox_rchBox.ReadOnly = True
+        Me.outputBox_rchBox.Size = New System.Drawing.Size(746, 476)
+        Me.outputBox_rchBox.TabIndex = 0
+        Me.outputBox_rchBox.Text = ""
+        '
+        'generate_btn
+        '
+        Me.generate_btn.Location = New System.Drawing.Point(674, 485)
+        Me.generate_btn.Name = "generate_btn"
+        Me.generate_btn.Size = New System.Drawing.Size(75, 23)
+        Me.generate_btn.TabIndex = 1
+        Me.generate_btn.Text = "Generate"
+        Me.generate_btn.UseVisualStyleBackColor = True
+        '
+        'save_btn
+        '
+        Me.save_btn.Location = New System.Drawing.Point(593, 485)
+        Me.save_btn.Name = "save_btn"
+        Me.save_btn.Size = New System.Drawing.Size(75, 23)
+        Me.save_btn.TabIndex = 2
+        Me.save_btn.Text = "Save"
+        Me.save_btn.UseVisualStyleBackColor = True
+        '
+        'multiOutput_chkBox
+        '
+        Me.multiOutput_chkBox.AutoSize = True
+        Me.multiOutput_chkBox.Location = New System.Drawing.Point(3, 489)
+        Me.multiOutput_chkBox.Name = "multiOutput_chkBox"
+        Me.multiOutput_chkBox.Size = New System.Drawing.Size(169, 17)
+        Me.multiOutput_chkBox.TabIndex = 3
+        Me.multiOutput_chkBox.Text = "Multi Output (Experimental)"
+        Me.multiOutput_chkBox.UseVisualStyleBackColor = True
+        '
+        'codeExporter_dialog
+        '
+        Me.codeExporter_dialog.CheckFileExists = True
+        Me.codeExporter_dialog.FileName = "GeneratedTrainers.txt"
+        Me.codeExporter_dialog.Filter = "Text Document (*.txt)|*.txt"
         '
         'Form1
         '
@@ -243,14 +487,18 @@ Partial Class Form1
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
-        Me.Text = "Form1"
+        Me.Text = "Showdown to Essentials Converter"
         Me.TabControl1.ResumeLayout(False)
         Me.pg_Trainer.ResumeLayout(False)
         Me.pg_Trainer.PerformLayout()
+        Me.pg_Output.ResumeLayout(False)
+        Me.pg_Output.PerformLayout()
         Me.pg_About.ResumeLayout(False)
         Me.pg_About.PerformLayout()
         CType(Me.gnuGPL3_picBox, System.ComponentModel.ISupportInitialize).EndInit()
         CType(Me.Logo_picBox, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.trainerItems_grp.ResumeLayout(False)
+        Me.trainerItems_grp.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
@@ -272,4 +520,28 @@ Partial Class Form1
     Friend WithEvents aboutAuthor_lbl As Label
     Friend WithEvents aboutTitle_lbl As Label
     Friend WithEvents Logo_picBox As PictureBox
+    Friend WithEvents existingBattle_chkBox As CheckBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents battleTeamID_lbl As Label
+    Friend WithEvents battleTeamID_txtBox As TextBox
+    Friend WithEvents trainerType_lbl As Label
+    Friend WithEvents trainerType_txtBox As TextBox
+    Friend WithEvents trainerUsesItems_chkBox As CheckBox
+    Friend WithEvents trainerItems_grp As GroupBox
+    Friend WithEvents numPoké_lbl As Label
+    Friend WithEvents numPoké_cmb As ComboBox
+    Friend WithEvents properSpellingNote_lbl As Label
+    Friend WithEvents itemEight_txtBox As TextBox
+    Friend WithEvents itemSeven_txtBox As TextBox
+    Friend WithEvents itemSix_txtBox As TextBox
+    Friend WithEvents itemFive_txtBox As TextBox
+    Friend WithEvents itemFour_txtBox As TextBox
+    Friend WithEvents itemThree_txtBox As TextBox
+    Friend WithEvents itemTwo_txtBox As TextBox
+    Friend WithEvents itemOne_txtBox As TextBox
+    Friend WithEvents multiOutput_chkBox As CheckBox
+    Friend WithEvents save_btn As Button
+    Friend WithEvents generate_btn As Button
+    Friend WithEvents outputBox_rchBox As RichTextBox
+    Friend WithEvents codeExporter_dialog As SaveFileDialog
 End Class
--- src/Showdown to Essentials Converter/Form1.vb
+++ src/Showdown to Essentials Converter/Form1.vb
@@ -1,3 +1,28 @@
 ﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Timer1.Start()
+        numPoké_cmb.SelectedIndex = 0
+    End Sub
 
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        If existingBattle_chkBox.Checked = True Then
+            battleTeamID_lbl.Enabled = True
+            battleTeamID_txtBox.Enabled = True
+        ElseIf existingBattle_chkBox.Checked = False Then
+            battleTeamID_lbl.Enabled = False
+            battleTeamID_txtBox.Enabled = False
+        End If
+        If trainerUsesItems_chkBox.Checked = True Then
+            trainerItems_grp.Enabled = True
+        ElseIf trainerUsesItems_chkBox.Checked = False Then
+            trainerItems_grp.Enabled = False
+        End If
+    End Sub
+
+    Private Sub save_btn_Click(sender As Object, e As EventArgs) Handles save_btn.Click
+        If codeExporter_dialog.ShowDialog = DialogResult.OK Then
+            My.Computer.FileSystem.WriteAllText(codeExporter_dialog.FileName, outputBox_rchBox.Text, False)
+            MessageBox.Show("File Saved at: " & codeExporter_dialog.FileName)
+        End If
+    End Sub
 End Class
