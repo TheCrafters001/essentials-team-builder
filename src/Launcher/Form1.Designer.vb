34f1295563430b9811a7d05ef3957e2645ca934f
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2021-07-16 13:02:49 -0400

    Redesigned the Launcher

--- src/Launcher/Form1.Designer.vb
+++ src/Launcher/Form1.Designer.vb
@@ -22,39 +22,19 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.pe17_pic = New System.Windows.Forms.PictureBox()
-        Me.pe18_pic = New System.Windows.Forms.PictureBox()
         Me.exit_btn = New System.Windows.Forms.Button()
         Me.Label1 = New System.Windows.Forms.Label()
-        CType(Me.pe17_pic, System.ComponentModel.ISupportInitialize).BeginInit()
-        CType(Me.pe18_pic, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.convertersList = New System.Windows.Forms.ListBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.convertersInfo_txt = New System.Windows.Forms.Label()
+        Me.launchSelected_btn = New System.Windows.Forms.Button()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
-        '
-        'pe17_pic
-        '
-        Me.pe17_pic.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.pe17_pic.Image = Global.Launcher.My.Resources.Resources.Essentials17Only
-        Me.pe17_pic.Location = New System.Drawing.Point(12, 12)
-        Me.pe17_pic.Name = "pe17_pic"
-        Me.pe17_pic.Size = New System.Drawing.Size(224, 110)
-        Me.pe17_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.pe17_pic.TabIndex = 0
-        Me.pe17_pic.TabStop = False
-        '
-        'pe18_pic
-        '
-        Me.pe18_pic.Cursor = System.Windows.Forms.Cursors.Hand
-        Me.pe18_pic.Image = Global.Launcher.My.Resources.Resources.Essentials18_19Only
-        Me.pe18_pic.Location = New System.Drawing.Point(243, 12)
-        Me.pe18_pic.Name = "pe18_pic"
-        Me.pe18_pic.Size = New System.Drawing.Size(224, 110)
-        Me.pe18_pic.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
-        Me.pe18_pic.TabIndex = 1
-        Me.pe18_pic.TabStop = False
         '
         'exit_btn
         '
-        Me.exit_btn.Location = New System.Drawing.Point(392, 133)
+        Me.exit_btn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.exit_btn.Location = New System.Drawing.Point(198, 347)
         Me.exit_btn.Name = "exit_btn"
         Me.exit_btn.Size = New System.Drawing.Size(75, 23)
         Me.exit_btn.TabIndex = 2
@@ -63,35 +43,73 @@ Partial Class Form1
         '
         'Label1
         '
-        Me.Label1.Location = New System.Drawing.Point(9, 133)
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.Location = New System.Drawing.Point(12, 316)
         Me.Label1.Name = "Label1"
-        Me.Label1.Size = New System.Drawing.Size(371, 35)
+        Me.Label1.Size = New System.Drawing.Size(177, 57)
         Me.Label1.TabIndex = 4
         Me.Label1.Text = "This is a non-profit fan-made tool. No copyright infringements intended. Please s" &
     "upport the official games!"
+        '
+        'convertersList
+        '
+        Me.convertersList.FormattingEnabled = True
+        Me.convertersList.Location = New System.Drawing.Point(12, 12)
+        Me.convertersList.Name = "convertersList"
+        Me.convertersList.Size = New System.Drawing.Size(177, 290)
+        Me.convertersList.TabIndex = 5
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Location = New System.Drawing.Point(195, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(202, 91)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 6
+        Me.PictureBox1.TabStop = False
+        '
+        'convertersInfo_txt
+        '
+        Me.convertersInfo_txt.Location = New System.Drawing.Point(195, 106)
+        Me.convertersInfo_txt.Name = "convertersInfo_txt"
+        Me.convertersInfo_txt.Size = New System.Drawing.Size(202, 196)
+        Me.convertersInfo_txt.TabIndex = 7
+        Me.convertersInfo_txt.Text = "Select a version on the left to get started." & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "<--------------"
+        '
+        'launchSelected_btn
+        '
+        Me.launchSelected_btn.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.launchSelected_btn.Location = New System.Drawing.Point(279, 347)
+        Me.launchSelected_btn.Name = "launchSelected_btn"
+        Me.launchSelected_btn.Size = New System.Drawing.Size(118, 23)
+        Me.launchSelected_btn.TabIndex = 8
+        Me.launchSelected_btn.Text = "Launch"
+        Me.launchSelected_btn.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(479, 167)
+        Me.ClientSize = New System.Drawing.Size(409, 382)
+        Me.Controls.Add(Me.launchSelected_btn)
+        Me.Controls.Add(Me.convertersInfo_txt)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.convertersList)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.exit_btn)
-        Me.Controls.Add(Me.pe18_pic)
-        Me.Controls.Add(Me.pe17_pic)
         Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
         Me.MaximizeBox = False
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Showdown to Essnetials Converter"
-        CType(Me.pe17_pic, System.ComponentModel.ISupportInitialize).EndInit()
-        CType(Me.pe18_pic, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents pe17_pic As PictureBox
-    Friend WithEvents pe18_pic As PictureBox
     Friend WithEvents exit_btn As Button
     Friend WithEvents Label1 As Label
+    Friend WithEvents convertersList As ListBox
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents convertersInfo_txt As Label
+    Friend WithEvents launchSelected_btn As Button
 End Class
--- src/Launcher/Form1.vb
+++ src/Launcher/Form1.vb
@@ -3,50 +3,58 @@
         Application.Exit()
     End Sub
 
-    Private Sub PictureBox1_Click(sender As Object, e As EventArgs) Handles pe17_pic.Click
-        Try
-            Dim pHelp As New ProcessStartInfo
-            pHelp.FileName = ".\Showdown to Essentials 17 Converter.exe"
-            pHelp.Arguments = ""
-            pHelp.UseShellExecute = True
-            pHelp.WindowStyle = ProcessWindowStyle.Normal
-            Dim proc As Process = Process.Start(pHelp)
-            Application.Exit()
-        Catch ex As Exception
-            MessageBox.Show(ex.ToString, "Error")
-        End Try
-    End Sub
-
-    Private Sub PictureBox2_Click(sender As Object, e As EventArgs) Handles pe18_pic.Click
-        Try
-            Dim pHelp As New ProcessStartInfo
-            pHelp.FileName = ".\Showdown to Essentials 18 & 19 Converter.exe"
-            pHelp.Arguments = ""
-            pHelp.UseShellExecute = True
-            pHelp.WindowStyle = ProcessWindowStyle.Normal
-            Dim proc As Process = Process.Start(pHelp)
-            Application.Exit()
-        Catch ex As Exception
-            MessageBox.Show(ex.ToString, "Error")
-        End Try
-    End Sub
-
-    Private Sub PictureBox1_MouseHover(sender As Object, e As EventArgs) Handles pe17_pic.MouseHover
-        pe17_pic.BackColor = Color.Gray
-    End Sub
-
-    Private Sub PictureBox1_MouseLeave(sender As Object, e As EventArgs) Handles pe17_pic.MouseLeave
-        pe17_pic.BackColor = SystemColors.Control
-    End Sub
-    Private Sub PictureBox2_MouseHover(sender As Object, e As EventArgs) Handles pe18_pic.MouseHover
-        pe18_pic.BackColor = Color.Gray
-    End Sub
-
-    Private Sub PictureBox2_MouseLeave(sender As Object, e As EventArgs) Handles pe18_pic.MouseLeave
-        pe18_pic.BackColor = SystemColors.Control
-    End Sub
-
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        If My.Computer.FileSystem.FileExists(".\Showdown to Essentials 18 & 19 Converter.exe") Then
+            convertersList.Items.Add("Showdown to Essentials 18 & 19 Converter")
+        End If
+        If My.Computer.FileSystem.FileExists(".\Showdown to Essentials 17 Converter.exe") Then
+            convertersList.Items.Add("Showdown to Essentials 17 Converter")
+        End If
+        launchSelected_btn.Enabled = False
+    End Sub
 
+    Private Sub convertersList_SelectedIndexChanged(sender As Object, e As EventArgs) Handles convertersList.SelectedIndexChanged
+        If convertersList.SelectedItem = "Showdown to Essentials 18 & 19 Converter" Then
+            PictureBox1.Image = My.Resources.Essentials18_19Only
+            convertersInfo_txt.Text = "Showdown to Essentials 18 & 19 Converter will let you convert Pokémon created in the Showdown format, and turn it into the Essentials format. This only works for Pokémon Essentials 18, 18.1, 19, and 19.1"
+            launchSelected_btn.Enabled = True
+        ElseIf convertersList.SelectedItem = "Showdown to Essentials 17 Converter" Then
+            PictureBox1.Image = My.Resources.Essentials17Only
+            convertersInfo_txt.Text = "Showdown to Essentials 17 Converter will let you convert Pokémon created in the Showdown format, and turn it into the Essentials format. This only works for Pokémon Essentials 17, 17.1, and 17.2"
+            launchSelected_btn.Enabled = True
+        Else
+            convertersInfo_txt.Text = "Select a version on the left to get started." & vbCrLf & "<--------------"
+            launchSelected_btn.Enabled = False
+        End If
+    End Sub
+
+    Private Sub launchSelected_btn_Click(sender As Object, e As EventArgs) Handles launchSelected_btn.Click
+        If convertersList.SelectedItem = "Showdown to Essentials 18 & 19 Converter" Then
+            Try
+                Dim pHelp As New ProcessStartInfo
+                pHelp.FileName = ".\Showdown to Essentials 18 & 19 Converter.exe"
+                pHelp.Arguments = ""
+                pHelp.UseShellExecute = True
+                pHelp.WindowStyle = ProcessWindowStyle.Normal
+                Dim proc As Process = Process.Start(pHelp)
+                Application.Exit()
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString, "Error")
+            End Try
+        ElseIf convertersList.SelectedItem = "Showdown to Essentials 17 Converter" Then
+            Try
+                Dim pHelp As New ProcessStartInfo
+                pHelp.FileName = ".\Showdown to Essentials 17 Converter.exe"
+                pHelp.Arguments = ""
+                pHelp.UseShellExecute = True
+                pHelp.WindowStyle = ProcessWindowStyle.Normal
+                Dim proc As Process = Process.Start(pHelp)
+                Application.Exit()
+            Catch ex As Exception
+                MessageBox.Show(ex.ToString, "Error")
+            End Try
+        Else
+            MessageBox.Show("You have selected an invalid version. Please select a valid version from the list.", "Error")
+        End If
     End Sub
 End Class
