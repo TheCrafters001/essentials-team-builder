d27b069cfa5fe1284773ac5989d4b9664e7d0e95
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2021-09-27 21:11:29 -0400

    Update Form1.Designer.vb

--- src/Showdown to Essentials 18 and 19 Converter/Form1.Designer.vb
+++ src/Showdown to Essentials 18 and 19 Converter/Form1.Designer.vb
@@ -23,19 +23,168 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.pages_tab = New Showdown_to_Essentials_18_and_19_Converter.BonfireTabControl()
+        Me.Trainer_pg = New System.Windows.Forms.TabPage()
+        Me.POne_pg = New System.Windows.Forms.TabPage()
+        Me.PTwo_pg = New System.Windows.Forms.TabPage()
+        Me.PThree_pg = New System.Windows.Forms.TabPage()
+        Me.PFour_pg = New System.Windows.Forms.TabPage()
+        Me.PFive_pg = New System.Windows.Forms.TabPage()
+        Me.PSix_pg = New System.Windows.Forms.TabPage()
+        Me.BallRef_pg = New System.Windows.Forms.TabPage()
+        Me.Output_pg = New System.Windows.Forms.TabPage()
+        Me.About_pg = New System.Windows.Forms.TabPage()
+        Me.OtherStuff_pg = New System.Windows.Forms.TabPage()
+        Me.pages_tab.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'pages_tab
+        '
+        Me.pages_tab.Controls.Add(Me.Trainer_pg)
+        Me.pages_tab.Controls.Add(Me.POne_pg)
+        Me.pages_tab.Controls.Add(Me.PTwo_pg)
+        Me.pages_tab.Controls.Add(Me.PThree_pg)
+        Me.pages_tab.Controls.Add(Me.PFour_pg)
+        Me.pages_tab.Controls.Add(Me.PFive_pg)
+        Me.pages_tab.Controls.Add(Me.PSix_pg)
+        Me.pages_tab.Controls.Add(Me.BallRef_pg)
+        Me.pages_tab.Controls.Add(Me.Output_pg)
+        Me.pages_tab.Controls.Add(Me.About_pg)
+        Me.pages_tab.Controls.Add(Me.OtherStuff_pg)
+        Me.pages_tab.Font = New System.Drawing.Font("Verdana", 8.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point)
+        Me.pages_tab.ItemSize = New System.Drawing.Size(0, 30)
+        Me.pages_tab.Location = New System.Drawing.Point(12, 12)
+        Me.pages_tab.Name = "pages_tab"
+        Me.pages_tab.SelectedIndex = 0
+        Me.pages_tab.Size = New System.Drawing.Size(1240, 657)
+        Me.pages_tab.TabIndex = 0
+        '
+        'Trainer_pg
+        '
+        Me.Trainer_pg.BackColor = System.Drawing.Color.White
+        Me.Trainer_pg.Location = New System.Drawing.Point(4, 34)
+        Me.Trainer_pg.Name = "Trainer_pg"
+        Me.Trainer_pg.Padding = New System.Windows.Forms.Padding(3)
+        Me.Trainer_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.Trainer_pg.TabIndex = 0
+        Me.Trainer_pg.Text = "Trainer Info"
+        '
+        'POne_pg
+        '
+        Me.POne_pg.BackColor = System.Drawing.Color.White
+        Me.POne_pg.Location = New System.Drawing.Point(4, 34)
+        Me.POne_pg.Name = "POne_pg"
+        Me.POne_pg.Padding = New System.Windows.Forms.Padding(3)
+        Me.POne_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.POne_pg.TabIndex = 1
+        Me.POne_pg.Text = "Pokémon 1"
+        '
+        'PTwo_pg
+        '
+        Me.PTwo_pg.BackColor = System.Drawing.Color.White
+        Me.PTwo_pg.Location = New System.Drawing.Point(4, 34)
+        Me.PTwo_pg.Name = "PTwo_pg"
+        Me.PTwo_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.PTwo_pg.TabIndex = 2
+        Me.PTwo_pg.Text = "Pokémon 2"
+        '
+        'PThree_pg
+        '
+        Me.PThree_pg.BackColor = System.Drawing.Color.White
+        Me.PThree_pg.Location = New System.Drawing.Point(4, 34)
+        Me.PThree_pg.Name = "PThree_pg"
+        Me.PThree_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.PThree_pg.TabIndex = 3
+        Me.PThree_pg.Text = "Pokémon 3"
+        '
+        'PFour_pg
+        '
+        Me.PFour_pg.BackColor = System.Drawing.Color.White
+        Me.PFour_pg.Location = New System.Drawing.Point(4, 34)
+        Me.PFour_pg.Name = "PFour_pg"
+        Me.PFour_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.PFour_pg.TabIndex = 4
+        Me.PFour_pg.Text = "Pokémon 4"
+        '
+        'PFive_pg
+        '
+        Me.PFive_pg.BackColor = System.Drawing.Color.White
+        Me.PFive_pg.Location = New System.Drawing.Point(4, 34)
+        Me.PFive_pg.Name = "PFive_pg"
+        Me.PFive_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.PFive_pg.TabIndex = 5
+        Me.PFive_pg.Text = "Pokémon 5"
+        '
+        'PSix_pg
+        '
+        Me.PSix_pg.BackColor = System.Drawing.Color.White
+        Me.PSix_pg.Location = New System.Drawing.Point(4, 34)
+        Me.PSix_pg.Name = "PSix_pg"
+        Me.PSix_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.PSix_pg.TabIndex = 6
+        Me.PSix_pg.Text = "Pokémon 6"
+        '
+        'BallRef_pg
+        '
+        Me.BallRef_pg.BackColor = System.Drawing.Color.White
+        Me.BallRef_pg.Location = New System.Drawing.Point(4, 34)
+        Me.BallRef_pg.Name = "BallRef_pg"
+        Me.BallRef_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.BallRef_pg.TabIndex = 7
+        Me.BallRef_pg.Text = "Pokéball ID Reference"
+        '
+        'Output_pg
+        '
+        Me.Output_pg.BackColor = System.Drawing.Color.White
+        Me.Output_pg.Location = New System.Drawing.Point(4, 34)
+        Me.Output_pg.Name = "Output_pg"
+        Me.Output_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.Output_pg.TabIndex = 8
+        Me.Output_pg.Text = "Output"
+        '
+        'About_pg
+        '
+        Me.About_pg.BackColor = System.Drawing.Color.White
+        Me.About_pg.Location = New System.Drawing.Point(4, 34)
+        Me.About_pg.Name = "About_pg"
+        Me.About_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.About_pg.TabIndex = 9
+        Me.About_pg.Text = "About"
+        '
+        'OtherStuff_pg
+        '
+        Me.OtherStuff_pg.BackColor = System.Drawing.Color.White
+        Me.OtherStuff_pg.Location = New System.Drawing.Point(4, 34)
+        Me.OtherStuff_pg.Name = "OtherStuff_pg"
+        Me.OtherStuff_pg.Size = New System.Drawing.Size(1232, 619)
+        Me.OtherStuff_pg.TabIndex = 10
+        Me.OtherStuff_pg.Text = "Other Stuff"
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(1264, 681)
+        Me.Controls.Add(Me.pages_tab)
         Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
         Me.Name = "Form1"
         Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
         Me.Text = "Showdown to Essentials 18 & 19 Converter"
+        Me.pages_tab.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
 
+    Friend WithEvents pages_tab As BonfireTabControl
+    Friend WithEvents Trainer_pg As TabPage
+    Friend WithEvents POne_pg As TabPage
+    Friend WithEvents PTwo_pg As TabPage
+    Friend WithEvents PThree_pg As TabPage
+    Friend WithEvents PFour_pg As TabPage
+    Friend WithEvents PFive_pg As TabPage
+    Friend WithEvents PSix_pg As TabPage
+    Friend WithEvents BallRef_pg As TabPage
+    Friend WithEvents Output_pg As TabPage
+    Friend WithEvents About_pg As TabPage
+    Friend WithEvents OtherStuff_pg As TabPage
 End Class
