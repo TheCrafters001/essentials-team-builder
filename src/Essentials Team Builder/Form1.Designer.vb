d440b2fcc7e6e8123fae2a405a2a82e500d623d7
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2023-11-07 21:54:08 -0500

    Recreate Project

--- src/Essentials Team Builder/Form1.Designer.vb
+++ src/Essentials Team Builder/Form1.Designer.vb
@@ -22,10 +22,167 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
-        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Text = "Form1"
+        GroupBox1 = New GroupBox()
+        GroupBox2 = New GroupBox()
+        TabControl1 = New TabControl()
+        TabPage1 = New TabPage()
+        TabPage2 = New TabPage()
+        TabControl2 = New TabControl()
+        TabPage3 = New TabPage()
+        TabPage4 = New TabPage()
+        TabControl3 = New TabControl()
+        TabPage5 = New TabPage()
+        TabPage6 = New TabPage()
+        GroupBox1.SuspendLayout()
+        TabControl1.SuspendLayout()
+        TabPage1.SuspendLayout()
+        TabPage2.SuspendLayout()
+        TabControl2.SuspendLayout()
+        TabControl3.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(TabControl1)
+        GroupBox1.Location = New Point(12, 12)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(757, 559)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "GroupBox1"
+        ' 
+        ' GroupBox2
+        ' 
+        GroupBox2.Location = New Point(775, 12)
+        GroupBox2.Name = "GroupBox2"
+        GroupBox2.Size = New Size(461, 559)
+        GroupBox2.TabIndex = 1
+        GroupBox2.TabStop = False
+        GroupBox2.Text = "GroupBox2"
+        ' 
+        ' TabControl1
+        ' 
+        TabControl1.Controls.Add(TabPage1)
+        TabControl1.Controls.Add(TabPage2)
+        TabControl1.Dock = DockStyle.Fill
+        TabControl1.Location = New Point(3, 19)
+        TabControl1.Name = "TabControl1"
+        TabControl1.SelectedIndex = 0
+        TabControl1.Size = New Size(751, 537)
+        TabControl1.TabIndex = 0
+        ' 
+        ' TabPage1
+        ' 
+        TabPage1.Controls.Add(TabControl2)
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Padding = New Padding(3)
+        TabPage1.Size = New Size(743, 509)
+        TabPage1.TabIndex = 0
+        TabPage1.Text = "TabPage1"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Controls.Add(TabControl3)
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Padding = New Padding(3)
+        TabPage2.Size = New Size(743, 509)
+        TabPage2.TabIndex = 1
+        TabPage2.Text = "TabPage2"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' TabControl2
+        ' 
+        TabControl2.Controls.Add(TabPage3)
+        TabControl2.Controls.Add(TabPage4)
+        TabControl2.Dock = DockStyle.Fill
+        TabControl2.Location = New Point(3, 3)
+        TabControl2.Name = "TabControl2"
+        TabControl2.SelectedIndex = 0
+        TabControl2.Size = New Size(737, 503)
+        TabControl2.TabIndex = 0
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.Location = New Point(4, 24)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Padding = New Padding(3)
+        TabPage3.Size = New Size(729, 475)
+        TabPage3.TabIndex = 0
+        TabPage3.Text = "TabPage3"
+        TabPage3.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage4
+        ' 
+        TabPage4.Location = New Point(4, 24)
+        TabPage4.Name = "TabPage4"
+        TabPage4.Padding = New Padding(3)
+        TabPage4.Size = New Size(192, 72)
+        TabPage4.TabIndex = 1
+        TabPage4.Text = "TabPage4"
+        TabPage4.UseVisualStyleBackColor = True
+        ' 
+        ' TabControl3
+        ' 
+        TabControl3.Controls.Add(TabPage5)
+        TabControl3.Controls.Add(TabPage6)
+        TabControl3.Dock = DockStyle.Fill
+        TabControl3.Location = New Point(3, 3)
+        TabControl3.Name = "TabControl3"
+        TabControl3.SelectedIndex = 0
+        TabControl3.Size = New Size(737, 503)
+        TabControl3.TabIndex = 0
+        ' 
+        ' TabPage5
+        ' 
+        TabPage5.Location = New Point(4, 24)
+        TabPage5.Name = "TabPage5"
+        TabPage5.Padding = New Padding(3)
+        TabPage5.Size = New Size(729, 475)
+        TabPage5.TabIndex = 0
+        TabPage5.Text = "TabPage5"
+        TabPage5.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage6
+        ' 
+        TabPage6.Location = New Point(4, 24)
+        TabPage6.Name = "TabPage6"
+        TabPage6.Padding = New Padding(3)
+        TabPage6.Size = New Size(192, 72)
+        TabPage6.TabIndex = 1
+        TabPage6.Text = "TabPage6"
+        TabPage6.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(1248, 583)
+        Controls.Add(GroupBox2)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        StartPosition = FormStartPosition.CenterScreen
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        TabControl1.ResumeLayout(False)
+        TabPage1.ResumeLayout(False)
+        TabPage2.ResumeLayout(False)
+        TabControl2.ResumeLayout(False)
+        TabControl3.ResumeLayout(False)
+        ResumeLayout(False)
     End Sub
 
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents TabControl1 As TabControl
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabControl2 As TabControl
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabControl3 As TabControl
+    Friend WithEvents TabPage5 As TabPage
+    Friend WithEvents TabPage6 As TabPage
+    Friend WithEvents GroupBox2 As GroupBox
 End Class
