24cba5da8f0cbf9b91415254ec90d3745a3da49f
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2023-11-07 22:03:38 -0500

    Update Basic Layout

--- src/Essentials Team Builder/Form1.Designer.vb
+++ src/Essentials Team Builder/Form1.Designer.vb
@@ -22,110 +22,122 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        GroupBox1 = New GroupBox()
-        GroupBox2 = New GroupBox()
-        TabControl1 = New TabControl()
+        editor_grp = New GroupBox()
+        editor_tab = New TabControl()
+        pg_trainer = New TabPage()
+        trainer_tab = New TabControl()
+        pg_basic = New TabPage()
+        pg_misc = New TabPage()
+        pg_items = New TabPage()
+        pg_pokemon = New TabPage()
+        TabControl3 = New TabControl()
         TabPage1 = New TabPage()
         TabPage2 = New TabPage()
-        TabControl2 = New TabControl()
         TabPage3 = New TabPage()
         TabPage4 = New TabPage()
-        TabControl3 = New TabControl()
         TabPage5 = New TabPage()
         TabPage6 = New TabPage()
-        GroupBox1.SuspendLayout()
-        TabControl1.SuspendLayout()
-        TabPage1.SuspendLayout()
-        TabPage2.SuspendLayout()
-        TabControl2.SuspendLayout()
+        livePreview_grp = New GroupBox()
+        RichTextBox1 = New RichTextBox()
+        editor_grp.SuspendLayout()
+        editor_tab.SuspendLayout()
+        pg_trainer.SuspendLayout()
+        trainer_tab.SuspendLayout()
+        pg_pokemon.SuspendLayout()
         TabControl3.SuspendLayout()
+        livePreview_grp.SuspendLayout()
         SuspendLayout()
         ' 
-        ' GroupBox1
+        ' editor_grp
         ' 
-        GroupBox1.Controls.Add(TabControl1)
-        GroupBox1.Location = New Point(12, 12)
-        GroupBox1.Name = "GroupBox1"
-        GroupBox1.Size = New Size(757, 559)
-        GroupBox1.TabIndex = 0
-        GroupBox1.TabStop = False
-        GroupBox1.Text = "GroupBox1"
+        editor_grp.Controls.Add(editor_tab)
+        editor_grp.Location = New Point(12, 12)
+        editor_grp.Name = "editor_grp"
+        editor_grp.Size = New Size(757, 559)
+        editor_grp.TabIndex = 0
+        editor_grp.TabStop = False
+        editor_grp.Text = "Editor"
         ' 
-        ' GroupBox2
+        ' editor_tab
         ' 
-        GroupBox2.Location = New Point(775, 12)
-        GroupBox2.Name = "GroupBox2"
-        GroupBox2.Size = New Size(461, 559)
-        GroupBox2.TabIndex = 1
-        GroupBox2.TabStop = False
-        GroupBox2.Text = "GroupBox2"
+        editor_tab.Controls.Add(pg_trainer)
+        editor_tab.Controls.Add(pg_pokemon)
+        editor_tab.Dock = DockStyle.Fill
+        editor_tab.Location = New Point(3, 19)
+        editor_tab.Name = "editor_tab"
+        editor_tab.SelectedIndex = 0
+        editor_tab.Size = New Size(751, 537)
+        editor_tab.TabIndex = 0
         ' 
-        ' TabControl1
+        ' pg_trainer
         ' 
-        TabControl1.Controls.Add(TabPage1)
-        TabControl1.Controls.Add(TabPage2)
-        TabControl1.Dock = DockStyle.Fill
-        TabControl1.Location = New Point(3, 19)
-        TabControl1.Name = "TabControl1"
-        TabControl1.SelectedIndex = 0
-        TabControl1.Size = New Size(751, 537)
-        TabControl1.TabIndex = 0
+        pg_trainer.Controls.Add(trainer_tab)
+        pg_trainer.Location = New Point(4, 24)
+        pg_trainer.Name = "pg_trainer"
+        pg_trainer.Padding = New Padding(3)
+        pg_trainer.Size = New Size(743, 509)
+        pg_trainer.TabIndex = 0
+        pg_trainer.Text = "Trainer"
+        pg_trainer.UseVisualStyleBackColor = True
         ' 
-        ' TabPage1
+        ' trainer_tab
         ' 
-        TabPage1.Controls.Add(TabControl2)
-        TabPage1.Location = New Point(4, 24)
-        TabPage1.Name = "TabPage1"
-        TabPage1.Padding = New Padding(3)
-        TabPage1.Size = New Size(743, 509)
-        TabPage1.TabIndex = 0
-        TabPage1.Text = "TabPage1"
-        TabPage1.UseVisualStyleBackColor = True
+        trainer_tab.Controls.Add(pg_basic)
+        trainer_tab.Controls.Add(pg_misc)
+        trainer_tab.Controls.Add(pg_items)
+        trainer_tab.Dock = DockStyle.Fill
+        trainer_tab.Location = New Point(3, 3)
+        trainer_tab.Name = "trainer_tab"
+        trainer_tab.SelectedIndex = 0
+        trainer_tab.Size = New Size(737, 503)
+        trainer_tab.TabIndex = 0
         ' 
-        ' TabPage2
+        ' pg_basic
         ' 
-        TabPage2.Controls.Add(TabControl3)
-        TabPage2.Location = New Point(4, 24)
-        TabPage2.Name = "TabPage2"
-        TabPage2.Padding = New Padding(3)
-        TabPage2.Size = New Size(743, 509)
-        TabPage2.TabIndex = 1
-        TabPage2.Text = "TabPage2"
-        TabPage2.UseVisualStyleBackColor = True
+        pg_basic.Location = New Point(4, 24)
+        pg_basic.Name = "pg_basic"
+        pg_basic.Size = New Size(729, 475)
+        pg_basic.TabIndex = 2
+        pg_basic.Text = "Basic Info"
+        pg_basic.UseVisualStyleBackColor = True
         ' 
-        ' TabControl2
+        ' pg_misc
         ' 
-        TabControl2.Controls.Add(TabPage3)
-        TabControl2.Controls.Add(TabPage4)
-        TabControl2.Dock = DockStyle.Fill
-        TabControl2.Location = New Point(3, 3)
-        TabControl2.Name = "TabControl2"
-        TabControl2.SelectedIndex = 0
-        TabControl2.Size = New Size(737, 503)
-        TabControl2.TabIndex = 0
+        pg_misc.Location = New Point(4, 24)
+        pg_misc.Name = "pg_misc"
+        pg_misc.Padding = New Padding(3)
+        pg_misc.Size = New Size(729, 475)
+        pg_misc.TabIndex = 0
+        pg_misc.Text = "Misc"
+        pg_misc.UseVisualStyleBackColor = True
         ' 
-        ' TabPage3
+        ' pg_items
         ' 
-        TabPage3.Location = New Point(4, 24)
-        TabPage3.Name = "TabPage3"
-        TabPage3.Padding = New Padding(3)
-        TabPage3.Size = New Size(729, 475)
-        TabPage3.TabIndex = 0
-        TabPage3.Text = "TabPage3"
-        TabPage3.UseVisualStyleBackColor = True
+        pg_items.Location = New Point(4, 24)
+        pg_items.Name = "pg_items"
+        pg_items.Padding = New Padding(3)
+        pg_items.Size = New Size(729, 475)
+        pg_items.TabIndex = 1
+        pg_items.Text = "Items"
+        pg_items.UseVisualStyleBackColor = True
         ' 
-        ' TabPage4
+        ' pg_pokemon
         ' 
-        TabPage4.Location = New Point(4, 24)
-        TabPage4.Name = "TabPage4"
-        TabPage4.Padding = New Padding(3)
-        TabPage4.Size = New Size(192, 72)
-        TabPage4.TabIndex = 1
-        TabPage4.Text = "TabPage4"
-        TabPage4.UseVisualStyleBackColor = True
+        pg_pokemon.Controls.Add(TabControl3)
+        pg_pokemon.Location = New Point(4, 24)
+        pg_pokemon.Name = "pg_pokemon"
+        pg_pokemon.Padding = New Padding(3)
+        pg_pokemon.Size = New Size(743, 509)
+        pg_pokemon.TabIndex = 1
+        pg_pokemon.Text = "Pokémon"
+        pg_pokemon.UseVisualStyleBackColor = True
         ' 
         ' TabControl3
         ' 
+        TabControl3.Controls.Add(TabPage1)
+        TabControl3.Controls.Add(TabPage2)
+        TabControl3.Controls.Add(TabPage3)
+        TabControl3.Controls.Add(TabPage4)
         TabControl3.Controls.Add(TabPage5)
         TabControl3.Controls.Add(TabPage6)
         TabControl3.Dock = DockStyle.Fill
@@ -135,6 +147,42 @@ Partial Class Form1
         TabControl3.Size = New Size(737, 503)
         TabControl3.TabIndex = 0
         ' 
+        ' TabPage1
+        ' 
+        TabPage1.Location = New Point(4, 24)
+        TabPage1.Name = "TabPage1"
+        TabPage1.Size = New Size(729, 475)
+        TabPage1.TabIndex = 2
+        TabPage1.Text = "Pokémon 1"
+        TabPage1.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage2
+        ' 
+        TabPage2.Location = New Point(4, 24)
+        TabPage2.Name = "TabPage2"
+        TabPage2.Size = New Size(729, 475)
+        TabPage2.TabIndex = 3
+        TabPage2.Text = "Pokémon 2"
+        TabPage2.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage3
+        ' 
+        TabPage3.Location = New Point(4, 24)
+        TabPage3.Name = "TabPage3"
+        TabPage3.Size = New Size(729, 475)
+        TabPage3.TabIndex = 4
+        TabPage3.Text = "Pokémon 3"
+        TabPage3.UseVisualStyleBackColor = True
+        ' 
+        ' TabPage4
+        ' 
+        TabPage4.Location = New Point(4, 24)
+        TabPage4.Name = "TabPage4"
+        TabPage4.Size = New Size(729, 475)
+        TabPage4.TabIndex = 5
+        TabPage4.Text = "Pokémon 4"
+        TabPage4.UseVisualStyleBackColor = True
+        ' 
         ' TabPage5
         ' 
         TabPage5.Location = New Point(4, 24)
@@ -142,7 +190,7 @@ Partial Class Form1
         TabPage5.Padding = New Padding(3)
         TabPage5.Size = New Size(729, 475)
         TabPage5.TabIndex = 0
-        TabPage5.Text = "TabPage5"
+        TabPage5.Text = "Pokémon 5"
         TabPage5.UseVisualStyleBackColor = True
         ' 
         ' TabPage6
@@ -150,39 +198,67 @@ Partial Class Form1
         TabPage6.Location = New Point(4, 24)
         TabPage6.Name = "TabPage6"
         TabPage6.Padding = New Padding(3)
-        TabPage6.Size = New Size(192, 72)
+        TabPage6.Size = New Size(729, 475)
         TabPage6.TabIndex = 1
-        TabPage6.Text = "TabPage6"
+        TabPage6.Text = "Pokémon 6"
         TabPage6.UseVisualStyleBackColor = True
+        ' 
+        ' livePreview_grp
+        ' 
+        livePreview_grp.Controls.Add(RichTextBox1)
+        livePreview_grp.Location = New Point(775, 12)
+        livePreview_grp.Name = "livePreview_grp"
+        livePreview_grp.Size = New Size(461, 559)
+        livePreview_grp.TabIndex = 1
+        livePreview_grp.TabStop = False
+        livePreview_grp.Text = "Live Preview"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.Dock = DockStyle.Fill
+        RichTextBox1.Location = New Point(3, 19)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.ReadOnly = True
+        RichTextBox1.Size = New Size(455, 537)
+        RichTextBox1.TabIndex = 0
+        RichTextBox1.Text = ""
         ' 
         ' Form1
         ' 
         AutoScaleDimensions = New SizeF(7F, 15F)
         AutoScaleMode = AutoScaleMode.Font
         ClientSize = New Size(1248, 583)
-        Controls.Add(GroupBox2)
-        Controls.Add(GroupBox1)
+        Controls.Add(livePreview_grp)
+        Controls.Add(editor_grp)
+        FormBorderStyle = FormBorderStyle.FixedSingle
         Name = "Form1"
         StartPosition = FormStartPosition.CenterScreen
         Text = "Form1"
-        GroupBox1.ResumeLayout(False)
-        TabControl1.ResumeLayout(False)
-        TabPage1.ResumeLayout(False)
-        TabPage2.ResumeLayout(False)
-        TabControl2.ResumeLayout(False)
+        editor_grp.ResumeLayout(False)
+        editor_tab.ResumeLayout(False)
+        pg_trainer.ResumeLayout(False)
+        trainer_tab.ResumeLayout(False)
+        pg_pokemon.ResumeLayout(False)
         TabControl3.ResumeLayout(False)
+        livePreview_grp.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
 
-    Friend WithEvents GroupBox1 As GroupBox
-    Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabControl2 As TabControl
-    Friend WithEvents TabPage3 As TabPage
-    Friend WithEvents TabPage4 As TabPage
-    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents editor_grp As GroupBox
+    Friend WithEvents editor_tab As TabControl
+    Friend WithEvents pg_trainer As TabPage
+    Friend WithEvents trainer_tab As TabControl
+    Friend WithEvents pg_misc As TabPage
+    Friend WithEvents pg_items As TabPage
+    Friend WithEvents pg_pokemon As TabPage
     Friend WithEvents TabControl3 As TabControl
     Friend WithEvents TabPage5 As TabPage
     Friend WithEvents TabPage6 As TabPage
-    Friend WithEvents GroupBox2 As GroupBox
+    Friend WithEvents livePreview_grp As GroupBox
+    Friend WithEvents pg_basic As TabPage
+    Friend WithEvents TabPage1 As TabPage
+    Friend WithEvents TabPage2 As TabPage
+    Friend WithEvents TabPage3 As TabPage
+    Friend WithEvents TabPage4 As TabPage
+    Friend WithEvents RichTextBox1 As RichTextBox
 End Class
