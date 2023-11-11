853ce158d56f4feaff83ca21a09a2d0737026482
TheCrafters001 <46639269+TheCrafters001@users.noreply.github.com>
2023-11-10 21:04:45 -0500

    Add new function codes

--- src/Essentials Team Builder/Form1.vb
+++ src/Essentials Team Builder/Form1.vb
@@ -21,95 +21,7 @@
     End Sub
 
     Private Sub essVersion_cmb_SelectedIndexChanged(sender As Object, e As EventArgs) Handles essVersion_cmb.SelectedIndexChanged
-        If essVersion_cmb.SelectedIndex = 0 Then ' Essentials 17
-            ' Custom Pokeball IDs
-            customBallIDs_chkBox.Enabled = True
-            ' customBallIDs_chkBox.Checked = False
-
-            ' Use Ability Index
-            useAbilityIndex_chkBox.Enabled = False
-            useAbilityIndex_chkBox.Checked = False
-
-            ' Quotes for Lose Text
-            loseTextQuotes_chkBox.Enabled = False
-            loseTextQuotes_chkBox.Checked = False
-
-            ' Super Shiny
-            allowSuperShiny_chk.Enabled = False
-            allowSuperShiny_chk.Checked = False
-
-
-        ElseIf essVersion_cmb.SelectedIndex = 1 Then ' Essentials 18
-            ' Custom Pokeball IDs
-            customBallIDs_chkBox.Enabled = True
-            ' customBallIDs_chkBox.Checked = False
-
-            ' Use Ability Index
-            useAbilityIndex_chkBox.Enabled = False
-            useAbilityIndex_chkBox.Checked = False
-
-            ' Quotes for Lose Text
-            loseTextQuotes_chkBox.Enabled = False
-            loseTextQuotes_chkBox.Checked = False
-
-            ' Super Shiny
-            allowSuperShiny_chk.Enabled = False
-            allowSuperShiny_chk.Checked = False
-
-
-        ElseIf essVersion_cmb.SelectedIndex = 2 Then ' Essentials 19
-            ' Custom Pokeball IDs
-            customBallIDs_chkBox.Enabled = True
-            ' customBallIDs_chkBox.Checked = False
-
-            ' Use Ability Index
-            useAbilityIndex_chkBox.Enabled = True
-            useAbilityIndex_chkBox.Checked = True
-
-            ' Quotes for Lose Text
-            loseTextQuotes_chkBox.Enabled = True
-            loseTextQuotes_chkBox.Checked = True
-
-            ' Super Shiny
-            allowSuperShiny_chk.Enabled = False
-            allowSuperShiny_chk.Checked = False
-
-
-        ElseIf essVersion_cmb.SelectedIndex = 3 Then ' Essentials 20
-            ' Custom Pokeball IDs
-            customBallIDs_chkBox.Enabled = True
-            ' customBallIDs_chkBox.Checked = False
-
-            ' Use Ability Index
-            useAbilityIndex_chkBox.Enabled = True
-            useAbilityIndex_chkBox.Checked = True
-
-            ' Quotes for Lose Text
-            loseTextQuotes_chkBox.Enabled = False
-            loseTextQuotes_chkBox.Checked = False
-
-            ' Super Shiny
-            allowSuperShiny_chk.Enabled = False
-            allowSuperShiny_chk.Checked = False
-
-
-        ElseIf essVersion_cmb.SelectedIndex = 4 Then ' Essentials 21
-            ' Custom Pokeball IDs
-            customBallIDs_chkBox.Enabled = True
-            ' customBallIDs_chkBox.Checked = False
-
-            ' Use Ability Index
-            useAbilityIndex_chkBox.Enabled = True
-            useAbilityIndex_chkBox.Checked = True
-
-            ' Quotes for Lose Text
-            loseTextQuotes_chkBox.Enabled = False
-            loseTextQuotes_chkBox.Checked = False
-
-            ' Super Shiny
-            allowSuperShiny_chk.Enabled = True
-            allowSuperShiny_chk.Checked = False
-        End If
+        ObjectCheck.EssVersion()
     End Sub
 
     Private Sub numPoké_cmb_SelectedIndexChanged(sender As Object, e As EventArgs) Handles numPoké_cmb.SelectedIndexChanged
@@ -169,5 +81,13 @@
         Else
 
         End If
+    End Sub
+
+    Private Sub allowSuperShiny_chk_CheckedChanged(sender As Object, e As EventArgs) Handles allowSuperShiny_chk.CheckedChanged
+        ObjectCheck.SuperShiny()
+    End Sub
+
+    Private Sub customBallIDs_chkBox_CheckedChanged(sender As Object, e As EventArgs) Handles customBallIDs_chkBox.CheckedChanged
+
     End Sub
 End Class
---
+++ src/Essentials Team Builder/scripts/ObjectCheck.vb
@@ -0,0 +1,122 @@
+﻿''' <summary>
+''' This function is meant to be ran if you run something like CheckedChanged or SelectedIndexChanged.
+''' </summary>
+Public Class ObjectCheck
+
+    ''' <summary>
+    ''' Used when the version of Essentials is changed.
+    ''' </summary>
+    Public Shared Sub EssVersion()
+        If Form1.essVersion_cmb.SelectedIndex = 0 Then ' Essentials 17
+            ' Custom Pokeball IDs
+            Form1.customBallIDs_chkBox.Enabled = True
+            ' customBallIDs_chkBox.Checked = False
+
+            ' Use Ability Index
+            Form1.useAbilityIndex_chkBox.Enabled = False
+            Form1.useAbilityIndex_chkBox.Checked = False
+
+            ' Quotes for Lose Text
+            Form1.loseTextQuotes_chkBox.Enabled = False
+            Form1.loseTextQuotes_chkBox.Checked = False
+
+            ' Super Shiny
+            Form1.allowSuperShiny_chk.Enabled = False
+            Form1.allowSuperShiny_chk.Checked = False
+
+
+        ElseIf Form1.essVersion_cmb.SelectedIndex = 1 Then ' Essentials 18
+            ' Custom Pokeball IDs
+            Form1.customBallIDs_chkBox.Enabled = True
+            ' customBallIDs_chkBox.Checked = False
+
+            ' Use Ability Index
+            Form1.useAbilityIndex_chkBox.Enabled = False
+            Form1.useAbilityIndex_chkBox.Checked = False
+
+            ' Quotes for Lose Text
+            Form1.loseTextQuotes_chkBox.Enabled = False
+            Form1.loseTextQuotes_chkBox.Checked = False
+
+            ' Super Shiny
+            Form1.allowSuperShiny_chk.Enabled = False
+            Form1.allowSuperShiny_chk.Checked = False
+
+
+        ElseIf Form1.essVersion_cmb.SelectedIndex = 2 Then ' Essentials 19
+            ' Custom Pokeball IDs
+            Form1.customBallIDs_chkBox.Enabled = True
+            ' customBallIDs_chkBox.Checked = False
+
+            ' Use Ability Index
+            Form1.useAbilityIndex_chkBox.Enabled = True
+            Form1.useAbilityIndex_chkBox.Checked = True
+
+            ' Quotes for Lose Text
+            Form1.loseTextQuotes_chkBox.Enabled = True
+            Form1.loseTextQuotes_chkBox.Checked = True
+
+            ' Super Shiny
+            Form1.allowSuperShiny_chk.Enabled = False
+            Form1.allowSuperShiny_chk.Checked = False
+
+
+        ElseIf Form1.essVersion_cmb.SelectedIndex = 3 Then ' Essentials 20
+            ' Custom Pokeball IDs
+            Form1.customBallIDs_chkBox.Enabled = True
+            ' customBallIDs_chkBox.Checked = False
+
+            ' Use Ability Index
+            Form1.useAbilityIndex_chkBox.Enabled = True
+            Form1.useAbilityIndex_chkBox.Checked = True
+
+            ' Quotes for Lose Text
+            Form1.loseTextQuotes_chkBox.Enabled = False
+            Form1.loseTextQuotes_chkBox.Checked = False
+
+            ' Super Shiny
+            Form1.allowSuperShiny_chk.Enabled = False
+            Form1.allowSuperShiny_chk.Checked = False
+
+
+        ElseIf Form1.essVersion_cmb.SelectedIndex = 4 Then ' Essentials 21
+            ' Custom Pokeball IDs
+            Form1.customBallIDs_chkBox.Enabled = True
+            ' customBallIDs_chkBox.Checked = False
+
+            ' Use Ability Index
+            Form1.useAbilityIndex_chkBox.Enabled = True
+            Form1.useAbilityIndex_chkBox.Checked = True
+
+            ' Quotes for Lose Text
+            Form1.loseTextQuotes_chkBox.Enabled = False
+            Form1.loseTextQuotes_chkBox.Checked = False
+
+            ' Super Shiny
+            Form1.allowSuperShiny_chk.Enabled = True
+            Form1.allowSuperShiny_chk.Checked = False
+        End If
+    End Sub
+
+
+    ''' <summary>
+    ''' Used to update the status of Super Shiny stuff.
+    ''' </summary>
+    Public Shared Sub SuperShiny()
+        If Form1.allowSuperShiny_chk.Checked = True Then
+            Form1.pokemon1SuperShiny_chkBox.Enabled = True
+        Else
+            Form1.pokemon1SuperShiny_chkBox.Enabled = False
+        End If
+    End Sub
+
+
+    Public Shared Sub customBallID()
+        If Form1.customBallIDs_chkBox.Checked = False Then
+            Form1.pokemon1PokeballID_cmb.DropDownStyle = ComboBoxStyle.DropDownList
+
+        ElseIf Form1.customBallIDs_chkBox.Checked = True Then
+            Form1.pokemon1PokeballID_cmb.DropDownStyle = ComboBoxStyle.DropDown
+        End If
+    End Sub
+End Class
